556f5cdc38e91c2fcd8ea25c9709483e69aa4943
heyymarco <yunuzk@gmail.com>
2021-08-03 18:13:57 +0700

    renamed Css.tsx => css-types.ts

---
+++ src/libs/css-types.ts
@@ -0,0 +1,24 @@
+import type { Property }   from 'csstype' // ts defs support for jss
+import type { JssStyle }   from 'jss'     // ts defs support for jss
+
+
+
+export type { Property as Prop }
+
+
+
+export namespace Cust {
+    export type Decl         = `--${string}`
+    export type RefOne       = `var(${Decl})`
+    export type Ref          = RefOne|`var(${Decl},${RefOne})`|`var(${Decl},${string})`
+    export type KeyframesRef = string
+    export type General      = string  | number // 'center', 'inline-flex', 12, '12px', 1.5, '150%'
+    export type Expr         = General | Ref | (General|Ref)[][] // [['clamp(', 12, 'var(--blah)', '100vw', ')']]
+}
+
+
+
+export namespace PropEx {
+    export type Keyframes = { [key: string]: JssStyle }
+    export type Length    = 0 | string
+}
