9539ecf81d8a72a476d62d9369233f327adefc00
heyymarco <yunuzk@gmail.com>
2021-08-05 21:16:58 +0700

    updated css-types

--- src/libs/css-types.ts
+++ src/libs/css-types.ts
@@ -1,5 +1,5 @@
-import type { Property }   from 'csstype' // ts defs support for jss
-import type { JssStyle }   from 'jss'     // ts defs support for jss
+import type { Property }    from 'csstype' // ts defs support for jss
+import type { Style }       from './jss-plugin-extend'
 
 // nodestrap (modular web components):
 import type {
@@ -17,13 +17,13 @@ export namespace Cust {
     export type RefOne       = `var(${Decl})`
     export type Ref          = RefOne|`var(${Decl},${RefOne})`|`var(${Decl},${string})`
     export type KeyframesRef = string
-    export type General      = string|number // 'center', 'inline-flex', 12, '12px', 1.5, '150%'
-    export type Expr         = General|Ref | (General|Ref)[][] // [['clamp(', 12, 'var(--blah)', '100vw', ')']]
+    export type General      = string|number
+    export type Expr         = General|Ref | (General|Ref)[] | (General|Ref)[][]
 }
 
 
 
 export namespace PropEx {
-    export type Keyframes = Dictionary<JssStyle>
+    export type Keyframes = Dictionary<Style>
     export type Length    = 0 | string
 }
