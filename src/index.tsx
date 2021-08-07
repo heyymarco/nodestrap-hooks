49d32ec61f17e07bd927b11c772726853bc22e09
heyymarco <yunuzk@gmail.com>
2021-08-07 23:43:20 +0700

    updated jss-plugin-global

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,14 @@ import reportWebVitals from './reportWebVitals';
 
 
 
-// import CssHooksApp from './apps/CssHooksApp';
-import CssConfigApp from './apps/CssConfigApp';
+import CssHooksApp from './apps/CssHooksApp';
+// import CssConfigApp from './apps/CssConfigApp';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <CssConfigApp />
+    <CssHooksApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/libs/jss-plugin-global.tsx
+++ src/libs/jss-plugin-global.tsx
@@ -1,32 +1,70 @@
 // jss   (builds css  using javascript):
-import type {
+import {
     Plugin,
-    JssStyle,
 
     Rule,
+    RuleList,
+    StyleSheet,
 }                           from 'jss'           // base technology of our nodestrap components
+import type {
+    Style,
+}                           from './jss-plugin-extend'
 
 
+
+const ruleGenerateId = (rule: Rule, sheet?: StyleSheet) => (rule as any).name ?? rule.key;
 
 class GlobalStyleRule {
     // BaseRule:
-    type        : string  = 'style' // for satisfying the jss-plugin-nested
+    type        : string  = 'style' // for satisfying `jss-plugin-nested`
     key         : string
     isProcessed : boolean = false   // required to avoid double processed
     options     : any
     renderable? : Object|null|void
 
-    style       : JssStyle
-    selector    : string  = ''      // for satisfying the jss-plugin-nested
+    // ContainerRule:
+    at          = '@global'
+    rules       : RuleList
+
+    // StyleRule:
+    selector    : string  = ''      // for satisfying `jss-plugin-nested`
 
     
     
-    constructor (key: string, style: JssStyle, options: any) {
+    constructor(key: string, style: Style, options: any) {
         this.key     = key;
-        this.style   = style;
         this.options = options;
 
-        options?.jss?.plugins?.onProcessRule(this); // let's `jss-plugin-extend` & `jss-plugin-nested` take care
+
+
+        this.rules = new RuleList({
+            ...options,
+            parent: this,
+        });
+
+
+        
+        for (const [propName, propValue] of Object.entries(style)) {
+            // exceptions:
+            if (propName.includes('&')) continue; // do not process nested rule
+            if (propName === 'extend')  continue; // do not process `extend` prop
+            
+            
+            
+            // because we're in `@global`, all prop names (with some exceptions above) will be recognized as selector expressions
+            const selectors = propName;
+            this.rules.add(selectors, propValue, {
+                ...options,
+                generateId : ruleGenerateId,
+
+                selector   : selectors,
+            });
+        } // for
+
+        
+        
+        // let's another plugins take care:
+        this.rules.process();
     }
 
     
@@ -34,15 +72,15 @@ class GlobalStyleRule {
     /**
      * Generates a CSS string.
      */
-    toString() {
-        return ''
+    toString(options?: any) {
+        return this.rules.toString(options);
     }
 }
 
 
 
 export default function pluginGlobal(): Plugin { return {
-    onCreateRule(key: string, style: JssStyle, options: any): Rule|any {
+    onCreateRule(key: string, style: Style, options: any): Rule|any {
         switch (key) {
             case '':
             case '@global':
