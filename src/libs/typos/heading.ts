c17d7356cd04e900abf7274c1651193ac7ef7e48
heyymarco <yunuzk@gmail.com>
2021-08-11 20:53:48 +0700

    typos => updated heading

--- src/libs/typos/heading.ts
+++ src/libs/typos/heading.ts
@@ -55,20 +55,20 @@ export const [cssProps, cssDecls, cssVals, cssConfig] = createCssConfig(() => {
         fontSize5         : [['calc(', 1.25, '*', gens.fontSize, ')']] as Prop.FontSize | Cust.Expr,
         fontSize6         : [['calc(', 1.00, '*', gens.fontSize, ')']] as Prop.FontSize | Cust.Expr,
         
-        fontFamily        : 'inherit'  as Prop.FontFamily[]                               | Cust.Ref,
-        fontWeight        : 500        as Prop.FontWeight                                 | Cust.Expr,
-        fontStyle         : 'inherit'  as Prop.FontStyle                                  | Cust.Ref,
-        textDecoration    : 'inherit'  as Prop.TextDecoration                             | Cust.Ref,
-        lineHeight        : 1.25       as Prop.LineHeight                                 | Cust.Expr,
+        fontFamily        : 'inherit' as Prop.FontFamily[]                              | Cust.Ref,
+        fontWeight        : 500       as Prop.FontWeight                                | Cust.Expr,
+        fontStyle         : 'inherit' as Prop.FontStyle                                 | Cust.Ref,
+        textDecoration    : 'inherit' as Prop.TextDecoration                            | Cust.Ref,
+        lineHeight        : 1.25      as Prop.LineHeight                                | Cust.Expr,
     
-        foreg             : 'inherit'  as Prop.Color                                      | Cust.Ref,
+        foreg             : 'inherit' as Prop.Color                                     | Cust.Ref,
         
-        marginBlockStart  : 0          as Prop.MarginBlockStart                           | Cust.Expr,
-        marginBlockEnd    : '0.75em'   as Prop.MarginBlockEnd                             | Cust.Expr,
-        marginInlineStart : 0          as Prop.MarginInlineStart                          | Cust.Expr,
-        marginInlineEnd   : 0          as Prop.MarginInlineEnd                            | Cust.Expr,
+        marginBlockStart  : 0         as Prop.MarginBlockStart                          | Cust.Expr,
+        marginBlockEnd    : '0.75em'  as Prop.MarginBlockEnd                            | Cust.Expr,
+        marginInlineStart : 0         as Prop.MarginInlineStart                         | Cust.Expr,
+        marginInlineEnd   : 0         as Prop.MarginInlineEnd                           | Cust.Expr,
 
-        subOpacity        : 0.8        as Prop.Opacity                                    | Cust.Expr,
+        subOpacity        : 0.8       as Prop.Opacity                                   | Cust.Expr,
     };
 }, { prefix: 'h' });
 export default cssProps;
