3fe5f03450f5fcf5e58df26f56513ab3413fed0f
heyymarco <yunuzk@gmail.com>
2021-08-11 21:34:10 +0700

    added borders & borders-radiuses

---
+++ src/libs/borders-radiuses.ts
@@ -0,0 +1,59 @@
+// nodestrap (modular web components):
+import type {
+    Prop,
+    Cust,
+}                           from './css-types'  // ts defs support for jss
+import type {
+    // general types:
+    Style
+}                           from './nodestrap'  // nodestrap core
+import createCssConfig      from './css-config' // Stores & retrieves configuration using *css custom properties* (css variables)
+
+
+
+// general types:
+type BorderRadius = Prop.BorderRadius | Cust.Expr
+
+
+
+// configs:
+export const [cssProps, cssDecls, cssVals, cssConfig] = createCssConfig(() => {
+    const basics = {
+        none    : '0px'     as BorderRadius,
+        sm      : '0.2rem'  as BorderRadius,
+        md      : '0.25rem' as BorderRadius,
+        lg      : '0.3rem'  as BorderRadius,
+        pill    : '50rem'   as BorderRadius,
+        circle  : '50%'     as BorderRadius,
+    };
+    
+    const defaults = {
+        default : basics.md as BorderRadius,
+    };
+    
+    return {
+        ...basics,
+        ...defaults,
+    };
+}, { prefix: 'bd-rd' });
+export { cssProps as radiuses, cssProps as default }
+const radiuses = cssProps;
+
+
+
+// export our mixins:
+const defaultRadius      : BorderRadius = radiuses?.default ?? radiuses?.md ?? '0.25rem';
+export const all         = (radius: BorderRadius = defaultRadius): Style => {
+    return {
+        borderRadius     : (((radius === defaultRadius) ? radiuses?.default : undefined) ?? radius) as BorderRadius,
+    };
+};
+export const topStart    = (radius: BorderRadius = defaultRadius): Style => ({ borderTopLeftRadius     : all(radius).borderRadius as Prop.BorderTopLeftRadius,     })
+export const topEnd      = (radius: BorderRadius = defaultRadius): Style => ({ borderTopRightRadius    : all(radius).borderRadius as Prop.BorderTopRightRadius,    })
+export const bottomStart = (radius: BorderRadius = defaultRadius): Style => ({ borderBottomLeftRadius  : all(radius).borderRadius as Prop.BorderBottomLeftRadius,  })
+export const bottomEnd   = (radius: BorderRadius = defaultRadius): Style => ({ borderBottomRightRadius : all(radius).borderRadius as Prop.BorderBottomRightRadius, })
+
+export const top         = (radius: BorderRadius = defaultRadius): Style => ((radius: BorderRadius) => ({ borderTopLeftRadius: radius as Prop.BorderTopLeftRadius, borderTopRightRadius: radius as Prop.BorderTopRightRadius,                                                                                                                                 }))(all(radius).borderRadius as BorderRadius)
+export const bottom      = (radius: BorderRadius = defaultRadius): Style => ((radius: BorderRadius) => ({                                                                                                                     borderBottomLeftRadius: radius as Prop.BorderBottomLeftRadius, borderBottomRightRadius: radius as Prop.BorderBottomRightRadius, }))(all(radius).borderRadius as BorderRadius)
+export const start       = (radius: BorderRadius = defaultRadius): Style => ((radius: BorderRadius) => ({ borderTopLeftRadius: radius as Prop.BorderTopLeftRadius,                                                            borderBottomLeftRadius: radius as Prop.BorderBottomLeftRadius,                                                                  }))(all(radius).borderRadius as BorderRadius)
+export const end         = (radius: BorderRadius = defaultRadius): Style => ((radius: BorderRadius) => ({                                                          borderTopRightRadius: radius as Prop.BorderTopRightRadius,                                                                borderBottomRightRadius: radius as Prop.BorderBottomRightRadius, }))(all(radius).borderRadius as BorderRadius)
---
+++ src/libs/borders.ts
@@ -0,0 +1,77 @@
+// nodestrap (modular web components):
+import type {
+    Prop,
+    Cust,
+}                           from './css-types'  // ts defs support for jss
+import type {
+    // general types:
+    Style
+}                           from './nodestrap'  // nodestrap core
+import createCssConfig      from './css-config' // Stores & retrieves configuration using *css custom properties* (css variables)
+import * as radius          from './borders-radiuses'
+import colors               from './colors'     // configurable colors & theming defs
+
+
+
+const radiuses = radius.radiuses;
+export { radiuses, radius };
+
+
+
+// general types:
+type BorderWidth = Prop.BorderWidth | Cust.Expr
+type BorderColor = Prop.BorderColor | Cust.Expr
+type BorderStyle = Prop.BorderStyle | Cust.Expr
+type Border      = Prop.Border      | Cust.Expr
+
+
+
+// configs:
+export const [cssProps, cssDecls, cssVals, cssConfig] = createCssConfig(() => {
+    const widths = {
+        none  : '0px' as BorderWidth,
+        hair  : '1px' as BorderWidth,
+        thin  : '2px' as BorderWidth,
+        bold  : '4px' as BorderWidth,
+    };
+    
+    const styles = {
+        color : colors.darkThin ?? 'currentColor' as BorderColor,
+        style : 'solid'                           as BorderStyle,
+    };
+    
+    const defaults = {
+        defaultWidth : widths.hair                                 as BorderWidth,
+        default      : [[styles.style, widths.hair, styles.color]] as Border,
+    };
+    
+    return {
+        ...widths,
+        ...styles,
+        ...defaults,
+    };
+}, { prefix: 'bd' });
+export { cssProps as borders, cssProps as default }
+const borders = cssProps;
+
+
+
+// export our mixins:
+// property of .default, .style, .defaultWidth, & .color "might" has deleted by user => use nullish op for safety => .?
+const defaultStyle     : BorderStyle = (cssVals?.default as [[BorderStyle, BorderWidth, BorderColor]])?.[0]?.[0] ?? borders?.style        ?? 'solid'
+const defaultWidth     : BorderWidth = (cssVals?.default as [[BorderStyle, BorderWidth, BorderColor]])?.[0]?.[1] ?? borders?.defaultWidth ?? borders?.hair ?? '1px'
+const defaultColor     : BorderColor = (cssVals?.default as [[BorderStyle, BorderWidth, BorderColor]])?.[0]?.[2] ?? borders?.color        ?? 'currentcolor'
+export const all       = (width: BorderWidth = defaultWidth): Style => {
+    return {
+        border         : (((width === defaultWidth) ? borders?.default : undefined) ?? [[defaultStyle, width, defaultColor]]) as Border,
+    };
+};
+export const top       = (width: BorderWidth = defaultWidth): Style => ({ borderTop    : all(width).border as Prop.BorderTop,    })
+export const bottom    = (width: BorderWidth = defaultWidth): Style => ({ borderBottom : all(width).border as Prop.BorderBottom, })
+export const left      = (width: BorderWidth = defaultWidth): Style => ({ borderLeft   : all(width).border as Prop.BorderLeft,   })
+export const right     = (width: BorderWidth = defaultWidth): Style => ({ borderRight  : all(width).border as Prop.BorderRight,  })
+
+export const notTop    = (width: BorderWidth = defaultWidth): Style => ((border: Border) => ({                                      borderBottom: border as Prop.BorderBottom, borderLeft: border as Prop.BorderLeft, borderRight: border as Prop.BorderRight, }))(all(width).border as Border)
+export const notBottom = (width: BorderWidth = defaultWidth): Style => ((border: Border) => ({ borderTop: border as Prop.BorderTop,                                            borderLeft: border as Prop.BorderLeft, borderRight: border as Prop.BorderRight, }))(all(width).border as Border)
+export const notLeft   = (width: BorderWidth = defaultWidth): Style => ((border: Border) => ({ borderTop: border as Prop.BorderTop, borderBottom: border as Prop.BorderBottom,                                        borderRight: border as Prop.BorderRight, }))(all(width).border as Border)
+export const notRight  = (width: BorderWidth = defaultWidth): Style => ((border: Border) => ({ borderTop: border as Prop.BorderTop, borderBottom: border as Prop.BorderBottom, borderLeft: border as Prop.BorderLeft,                                          }))(all(width).border as Border)
