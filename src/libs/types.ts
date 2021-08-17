2a603910cbd386ddaa28b2261930539dab541797
heyymarco <yunuzk@gmail.com>
2021-08-17 21:38:01 +0700

    updated types

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,12 +1,13 @@
-export type Optional<T>                = T|null|undefined
-export type SingleOrArray<T>           = T|T[]
+export type Optional<T>                  = T|null|undefined
+export type SingleOrArray<T>             = T|T[]
 
-export type DeepArray<T>               = Array<T|DeepArray<T>>
-export type SingleOrDeepArray<T>       = T|DeepArray<T>
+export type DeepArray<T>                 = Array<T|DeepArray<T>>
+export type SingleOrDeepArray<T>         = T|DeepArray<T>
 
-export type Factory<TProduct>          = () => TProduct
-export type ProductOrFactory<TProduct> = TProduct|Factory<TProduct>
+export type Factory<T>                   = () => T
+export type ProductOrFactory<T>          = T|Factory<T>
+export type ProductOrFactoryDeepArray<T> = ProductOrFactory<T>|DeepArray<ProductOrFactory<T>>
 
-export type Dictionary<TValue>         = { [key: string]: TValue }
-export type ValueOf<TCollection>       = TCollection[keyof TCollection]
-export type DictionaryOf<TCollection>  = Dictionary<ValueOf<TCollection>>
+export type Dictionary<TValue>           = { [key: string]: TValue }
+export type ValueOf<TCollection>         = TCollection[keyof TCollection]
+export type DictionaryOf<TCollection>    = Dictionary<ValueOf<TCollection>>
