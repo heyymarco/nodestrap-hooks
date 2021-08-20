bc06021f0b288d48c155cea97ac9e0140c9af424
heyymarco <yunuzk@gmail.com>
2021-08-20 15:43:48 +0700

    updated Content

--- src/libs/Content.tsx
+++ src/libs/Content.tsx
@@ -64,7 +64,7 @@ export const usesContent = () => {
     
     return composition([
         imports([
-            // base:
+            // bases:
             usesIndicator(),
             
             // layouts:
