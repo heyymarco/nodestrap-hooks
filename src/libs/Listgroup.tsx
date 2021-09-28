ba3a79fa82d69e266373251455e28f6a9e6ad358
heyymarco <yunuzk@gmail.com>
2021-09-28 15:36:38 +0700

    updated Listgroup

--- src/libs/Listgroup.tsx
+++ src/libs/Listgroup.tsx
@@ -381,7 +381,7 @@ export const usesListgroupLayout = () => {
             // layouts:
          // display        : 'flex',    // customizable orientation // already defined in variant `.block`/`.inline`
          // flexDirection  : 'column',  // customizable orientation // already defined in variant `.block`/`.inline`
-            justifyContent : 'start',   // if wrappers are not growable, the remaining space (if any) placed at the end, and if no space available => the first wrapper should be visible first
+            justifyContent : 'start',   // if wrappers are not growable, the excess space (if any) placed at the end, and if no sufficient space available => the first wrapper should be visible first
             alignItems     : 'stretch', // wrappers width are 100% of the parent (for variant `.block`) or height (for variant `.inline`)
             
             
@@ -396,7 +396,7 @@ export const usesListgroupLayout = () => {
                 layout({
                     // layouts:
                     display        : 'flex',    // use block flexbox, so it takes the entire Listgroup's width
-                    justifyContent : 'start',   // if listItems are not growable, the remaining space (if any) placed at the end, and if no space available => the first listItem should be visible first
+                    justifyContent : 'start',   // if listItems are not growable, the excess space (if any) placed at the end, and if no sufficient space available => the first listItem should be visible first
                     alignItems     : 'stretch', // listItems width are 100% of the wrapper (for variant `.block`) or height (for variant `.inline`)
                     
                     
