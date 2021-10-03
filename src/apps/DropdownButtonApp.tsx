f448d27758bb57f3d818748e23d282c3f68a6b23
heyymarco <yunuzk@gmail.com>
2021-10-03 15:35:55 +0700

    DropdownButton done

---
+++ src/apps/DropdownButtonApp.tsx
@@ -0,0 +1,189 @@
+import './App.css';
+
+import {
+	default as React,
+    useState,
+}                          from 'react';
+
+import Container from '../libs/Container';
+import {
+	ThemeName,
+	SizeName,
+} 					from '../libs/BasicComponent';
+import Button   from '../libs/Button';
+import DropdownButton, {OrientationName} from '../libs/DropdownButton';
+
+
+
+function App() {
+    const themes = [undefined,'primary','secondary','success','info','warning','danger','light','dark'];
+    const [theme, 	   setTheme     ] = useState<ThemeName|undefined>('primary');
+
+    const sizes = ['sm', undefined, 'lg'];
+	const [size, 	   setSize      ] = useState<SizeName|undefined>(undefined);
+
+	const [enableGrad, setEnableGrad] = useState(false);
+	const [outlined,   setOutlined  ] = useState(false);
+
+	const milds = [false, undefined, true];
+	const [mild,       setMild      ] = useState<boolean|undefined>(undefined);
+
+	const [enabled,    setEnabled   ] = useState(true);
+	const [active,      setActive   ] = useState(false);
+
+	const orientations = [undefined, 'block', 'inline'];
+	const [orientation,    setOrientation     ] = useState<OrientationName|undefined>(undefined);
+
+	
+
+    return (
+        <div className="App">
+            <Container>
+				<Button onClick={(e) => {
+					if (e.buttons) return; // no other button(s) being pressed
+					
+					setActive(!active);
+				}}>Toggle DropdownButton</Button>
+				<DropdownButton
+					// popupPlacement='bottom'
+					
+					theme={theme} size={size} gradient={enableGrad}
+					outlined={outlined} mild={mild}
+
+					enabled={enabled} active={active}
+					orientation={orientation}
+					onActiveChange={(newActive) => {
+						// if (document.activeElement === targetButtonRef.current) return; // dropdown lost focus because the toggler button got focus => ignore
+						setActive(newActive);
+					}}
+
+					buttonChildren='show menu'
+				>
+					<p>Hello world!</p>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                </DropdownButton>
+
+				<DropdownButton
+					// popupPlacement='bottom'
+					
+					theme={theme} size={size} gradient={enableGrad}
+					outlined={outlined} mild={mild}
+
+					enabled={enabled}
+					orientation={orientation}
+
+					buttonChildren='show menu'
+				>
+					<img style={{width: '200px'}} src="https://assets.codepen.io/12005/windmill.jpg" alt="A windmill" />
+                </DropdownButton>
+                <hr style={{flexBasis: '100%'}} />
+				<p>
+					Theme:
+					{
+						themes.map(th =>
+							<label key={th ?? ''}>
+								<input type='radio'
+									value={th}
+									checked={theme===th}
+									onChange={(e) => setTheme(e.target.value as ThemeName || undefined)}
+								/>
+								{`${th}`}
+							</label>
+						)
+					}
+				</p>
+				<p>
+					Size:
+					{
+						sizes.map(sz =>
+							<label key={sz ?? ''}>
+								<input type='radio'
+									value={sz}
+									checked={size===sz}
+									onChange={(e) => setSize(e.target.value as SizeName || undefined)}
+								/>
+								{`${sz}`}
+							</label>
+						)
+					}
+				</p>
+				<p>
+					<label>
+						<input type='checkbox'
+							checked={enableGrad}
+							onChange={(e) => setEnableGrad(e.target.checked)}
+						/>
+						enable gradient
+					</label>
+				</p>
+				<p>
+					<label>
+						<input type='checkbox'
+							checked={outlined}
+							onChange={(e) => setOutlined(e.target.checked)}
+						/>
+						outlined
+					</label>
+				</p>
+				<p>
+					Mild:
+					{
+						milds.map(mi =>
+							<label key={`${mi}`}>
+								<input type='radio'
+									value={`${mi}`}
+									checked={mild===mi}
+									onChange={(e) => setMild((() => {
+										const value = e.target.value;
+										if (!value) return undefined;
+										switch (value) {
+											case 'true' : return true;
+											case 'false': return false;
+											default     : return undefined;
+										} // switch
+									})())}
+								/>
+								{`${mi ?? 'unset'}`}
+							</label>
+						)
+					}
+				</p>
+				<p>
+					<label>
+						<input type='checkbox'
+							checked={enabled}
+							onChange={(e) => setEnabled(e.target.checked)}
+						/>
+						enabled
+					</label>
+				</p>
+				<p>
+					<label>
+						<input type='checkbox'
+							checked={active}
+							onChange={(e) => setActive(e.target.checked)}
+						/>
+						active
+					</label>
+				</p>
+				<p>
+					OrientationStyle:
+					{
+						orientations.map(ori =>
+							<label key={ori ?? ''}>
+								<input type='radio'
+									value={ori}
+									checked={orientation===ori}
+									onChange={(e) => setOrientation((e.target.value || undefined) as (OrientationName|undefined))}
+								/>
+								{`${ori}`}
+							</label>
+						)
+					}
+				</p>
+            </Container>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/libs/DropdownButton.tsx
@@ -0,0 +1,204 @@
+// react (builds html using javascript):
+import {
+    default as React,
+    useRef,
+}                           from 'react'         // base technology of our nodestrap components
+
+// cssfn:
+import {
+    setElmRef,
+}                           from './react-cssfn' // cssfn for react
+import type {
+    // hooks:
+    OrientationName,
+    OrientationVariant,
+}                           from './BasicComponent'
+import {
+    // hooks:
+    TogglerActiveProps,
+    useTogglerActive,
+}                           from './Indicator'
+import {
+    // general types:
+    PopupPlacement,
+    PopupModifier,
+    PopupPosition,
+    
+    
+    
+    // react components:
+    DropdownCloseType,
+    
+    DropdownProps,
+    Dropdown,
+}                           from './Dropdown'
+import {
+    // react components:
+    ButtonIconProps,
+    ButtonIcon,
+}                           from './ButtonIcon'
+
+
+
+// react components:
+
+export type { DropdownCloseType }
+
+
+
+export interface DropdownButtonProps<TCloseType = DropdownCloseType>
+    extends
+        DropdownProps<HTMLButtonElement, TCloseType>,
+        TogglerActiveProps<TCloseType>,
+        
+        ButtonIconProps
+{
+    // layouts:
+    buttonOrientation? : OrientationName
+    
+    
+    // accessibilities:
+    label?          : string
+    
+    
+    // children:
+    buttonChildren? : React.ReactNode
+}
+export function DropdownButton<TCloseType = DropdownCloseType>(props: DropdownButtonProps<TCloseType>) {
+    // states:
+    const [isActive, setActive] = useTogglerActive(props);
+    
+    
+    
+    // rest props:
+    const {
+        // essentials:
+        elmRef,
+        
+        
+        // accessibilities:
+        defaultActive,  // delete, already handled by `useTogglerActive`
+        active,         // delete, already handled by `useTogglerActive`
+        onActiveChange, // delete, already handled by `useTogglerActive`
+        
+        label,
+        
+        
+        // layouts:
+        orientation       = 'block',
+        buttonOrientation = (orientation === 'inline') ? 'block' : 'inline',
+        
+        
+        // appearances:
+        icon         = (() => { // from IconProps
+            switch (orientation) {
+                // todo: detect LTR or RTL
+                case 'inline' :
+                    return 'dropright';
+                
+                case 'block'  :
+                default       :
+                    return 'dropdown';
+            } // switch
+        })(),
+        iconPosition = 'end',   // from IconProps
+        
+        
+        // children:
+        children,
+        buttonChildren,
+    ...restProps} = props;
+    
+    
+    
+    // handlers:
+    const handleToggleActive = () => {
+        setActive(!isActive); // toggle active
+    }
+    
+    
+    
+    // dom effects:
+    const buttonRef = useRef<HTMLButtonElement|null>(null);
+    
+    
+    
+    // jsx:
+    return (
+        <>
+            <ButtonIcon
+                // other props:
+                {...restProps}
+                
+                
+                // essentials:
+                elmRef={(elm) => {
+                    setElmRef(elmRef, elm);
+                    setElmRef(buttonRef, elm);
+                }}
+                
+                
+                // accessibilities:
+                {...{
+                    label,
+                }}
+                
+                
+                // layouts:
+                orientation={buttonOrientation}
+                
+                
+                // appearances:
+                {...{
+                    icon,
+                    iconPosition,
+                }}
+                
+                
+                
+                // children:
+                children={buttonChildren}
+                
+                
+                // events:
+                onClick={(e) => {
+                    if (!e.defaultPrevented) {
+                        handleToggleActive();
+                        e.preventDefault();
+                    } // if
+                }}
+            />
+            <Dropdown<HTMLElement, TCloseType>
+                // other props:
+                {...restProps}
+                
+                
+                // popups:
+                targetRef={props.targetRef ?? buttonRef}
+                
+                
+                // accessibilities:
+                active={isActive}
+                onActiveChange={(newActive, closeType) => {
+                    if (onActiveChange) { // controllable
+                        onActiveChange(newActive, closeType as unknown as TCloseType);
+                    }
+                    else { // uncontrollable
+                        setActive(newActive);
+                    } // if
+                }}
+                
+                
+                // layouts:
+                orientation={orientation}
+            >
+                { children }
+            </Dropdown>
+        </>
+    );
+}
+DropdownButton.prototype = ButtonIcon.prototype; // mark as ButtonIcon compatible
+export { DropdownButton as default }
+
+export type { PopupPlacement, PopupModifier, PopupPosition }
+export type { OrientationName, OrientationVariant }
