b8eafdf47c7ab46315faac3f4746677cf45d537e
heyymarco <yunuzk@gmail.com>
2021-10-05 19:25:34 +0700

    updated Basic

--- src/libs/Basic.tsx
+++ src/libs/Basic.tsx
@@ -973,7 +973,7 @@ const propsManager  = {
     unregisterAnim      : (item: Cust.Ref) => setsAnim.delete(item),
 } as const;
 
-const convertRefToDecl = (ref: Cust.Ref): Cust.Decl|null => (ref.match(/(?<=var\(\s*)--[\w-]+(?=\s*(?:,[^)]*)?\))/)?.[0] ?? null) as Cust.Decl|null;
+export const convertRefToDecl = (ref: Cust.Ref): Cust.Decl|null => (ref.match(/(?<=var\(\s*)--[\w-]+(?=\s*(?:,[^)]*)?\))/)?.[0] ?? null) as Cust.Decl|null;
 export const usesAnim = () => {
     return [
         () => composition([
