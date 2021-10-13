10adf2b4c620fa58ca2b15ba27b72eb6588a44b5
heyymarco <yunuzk@gmail.com>
2021-10-13 14:52:33 +0700

    updated Card

--- src/libs/Card.tsx
+++ src/libs/Card.tsx
@@ -206,7 +206,7 @@ export const usesCardLayout = () => {
             
             borderWidth            : borderStrokeRefs.borderWidth,            // overwrite width prop
             
-            borderRadius           : null,                                    // delete short prop
+            borderRadius           : undefined as unknown as null,            // delete short prop
             borderStartStartRadius : borderRadiusRefs.borderStartStartRadius, // overwrite radius prop
             borderStartEndRadius   : borderRadiusRefs.borderStartEndRadius,   // overwrite radius prop
             borderEndStartRadius   : borderRadiusRefs.borderEndStartRadius,   // overwrite radius prop
