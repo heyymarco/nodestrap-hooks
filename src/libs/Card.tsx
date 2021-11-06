77a8b6e6464cddfc2768ae666afd1f4c90497121
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2021-11-06 21:25:14 +0700

    updated Card

--- src/libs/Card.tsx
+++ src/libs/Card.tsx
@@ -40,6 +40,7 @@ import {
 import {
     // hooks:
     usesSizeVariant,
+    
     OrientationName,
     OrientationRuleOptions,
     defaultBlockOrientationRuleOptions,
@@ -47,6 +48,7 @@ import {
     usesOrientationRule,
     OrientationVariant,
     useOrientationVariant,
+    
     usesBorder,
     usesBorderStroke,
     expandBorderStroke,
