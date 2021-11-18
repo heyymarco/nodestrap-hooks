600a6555ad275eb45797e56c62ced4c0490a8f4e
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2021-11-18 23:28:11 +0700

    updated Dropdown

--- src/libs/Dropdown.tsx
+++ src/libs/Dropdown.tsx
@@ -35,6 +35,11 @@ import {
 
 // nodestrap utilities:
 import {
+    // utilities:
+    isTypeOf,
+    setRef,
+}                           from './utilities'
+import {
     stripoutFocusableElement,
 }                           from './stripouts'
 
@@ -43,12 +48,6 @@ import {
     // react components:
     ElementProps,
     Element,
-    
-    
-    
-    // utilities:
-    isTypeOf,
-    setRef,
 }                           from './Element'
 import {
     // hooks:
