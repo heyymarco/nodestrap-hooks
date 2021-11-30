dfd3e8666df1a52884856c47d595f7d6e552ea58
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2021-12-01 01:14:22 +0700

    updated jss-plugin-short

--- src/libs/jss-plugin-short.ts
+++ src/libs/jss-plugin-short.ts
@@ -9,7 +9,7 @@ import type {
 
 
 
-const shorts: { [key: string]: string } = {
+const shorts: Record<string, string> = {
     foreg       : 'color',
     backg       : 'background',
     'backgClip' : 'backgroundClip',
@@ -21,26 +21,50 @@ const shorts: { [key: string]: string } = {
     'gapBlock'  : 'rowGap',
 };
 
-const onProcessStyle = (style: JssStyle & { [key: string]: JssStyle[keyof JssStyle] }, rule: Rule, sheet?: StyleSheet): JssStyle => {
-    // convert LiteralObject to Array, so the prop order preserved:
+const renameProps = (style: JssStyle): JssStyle => {
+    // stores the style's entries __only_if__ the modification is needed:
     let styleArrLazy : [string, any][]|null = null;
     
-    for (const [propName, index] of Object.keys(style).map((key, index) => [key, index] as const)) {
+    
+    
+    for (const [propName, propIndex] of Object.keys(style).map((propName, propIndex) => [propName, propIndex] as const)) {
         if (propName in shorts) {
-            // initialize styleArrLazy:
+            /*
+                initialize styleArrLazy (if was not initialized).
+                
+                convert LiteralObject to Array, so the prop order preserved.
+                the order of the prop is guaranteed to be __preserved__
+                so to rename the prop, just search with known `propIndex`.
+            */
             if (!styleArrLazy) styleArrLazy = Object.entries(style);
             
             // set the expanded propName:
-            styleArrLazy[index][0] = shorts[propName];
+            styleArrLazy[propIndex][0] = shorts[propName];
         } // if
     } // for
     
     
     
-    if (styleArrLazy) return Object.fromEntries(styleArrLazy); // return the modified
-    return style; // return the original
+    if (styleArrLazy) return Object.fromEntries(styleArrLazy); // has changed => return the modified
+    return style; // no changes => return the original
+};
+
+
+
+const onProcessStyle = (style: JssStyle, rule: Rule, sheet?: StyleSheet): JssStyle => {
+    return renameProps(style);
+}
+
+const onChangeValue = (propValue: string, propName: string, rule: Rule): string|null|false => {
+    if (!(propName in shorts)) return propValue;
+    
+    
+    
+    (rule as any).prop(shorts[propName], propValue); // add the modified name
+    return null; // delete the original name
 };
 
 export default function pluginShort(): Plugin { return {
     onProcessStyle,
+    onChangeValue,
 }}
