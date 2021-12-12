2c2bf8fb5b4b1140ddedb04bf5ddbc239a63b335
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2021-12-12 14:26:23 +0700

    updated css-selector

--- src/libs/css-selector.ts
+++ src/libs/css-selector.ts
@@ -365,10 +365,10 @@ export const parseSelectors = (expression: string): SelectorList|null => {
         
         const value = expression.substring(originPos + 1, pos - 1); // excludes the opening_quoteChar & closing_quoteChar
         if (quoteChar === "'") { // single quoteChar
-            return value.replaceAll(/(?<!\\)"/g, '\\"'); // escape the unescaped double quoteChar, so both single & double quoteChar are escaped
+            return value.replace(/(?<!\\)"/g, '\\"'); // escape the unescaped double quoteChar, so both single & double quoteChar are escaped
         }
         else { // double quoteChar
-            return value.replaceAll(/(?<!\\)'/g, "\\'"); // escape the unescaped single quoteChar, so both single & double quoteChar are escaped
+            return value.replace(/(?<!\\)'/g, "\\'"); // escape the unescaped single quoteChar, so both single & double quoteChar are escaped
         } // if
     };
     const parseString = (): string|null => {
