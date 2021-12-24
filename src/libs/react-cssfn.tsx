ab738b8af674ab515fda772ad237ac959514f805
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2021-12-24 21:53:57 +0700

    updated react-cssfn

--- src/libs/react-cssfn.tsx
+++ src/libs/react-cssfn.tsx
@@ -2,6 +2,7 @@
 import {
     useEffect,
     useLayoutEffect,
+    useState,
 }                           from 'react'         // base technology of our cssfn components
 
 // jss:
@@ -46,34 +47,51 @@ const useIsomorphicLayoutEffect = isBrowser ? useLayoutEffect : useEffect;
 const sheetManager = new SheetsManager(); // caches & manages sheets usage, attached to dom when in use and detached from dom when not in use
 export const createUseJssSheet = <TClassName extends ClassName = ClassName>(styles: ProductOrFactory<Styles<TClassName>>, sheetId?: string): Factory<Classes<TClassName>> => {
     const sheetIdObj = {}; // a simple object for the sheet's identifier (by reference)
+    // console.log('declare sheet', sheetId);
     
     
     
     return (): Classes<TClassName> => {
-        const sheet = ( // no need to use `useMemo` because fetching from `sheetManager` is inexpensive
-            // take from an existing cached sheet (if any):
-            sheetManager.get(sheetIdObj) // inexpensive operation
-            ??
-            // or create a new one:
-            (() => { // expensive operation
-                // create a new sheet using our pre-configured `customJss`:
-                const newSheet = createJssSheet(styles, sheetId);
-                
-                
-                
-                // register to `sheetManager` to be cached and also to be able to attach/detach to/from dom:
-                sheetManager.add(sheetIdObj, newSheet);
-
-                
-                
-                // here the ready to use sheet:
-                return newSheet;
-            })()
+        // no need to use `useMemo` because fetching from `sheetManager` is inexpensive
+        
+        
+        
+        // take from an existing cached sheet (if any):
+        const sheet = sheetManager.get(sheetIdObj);
+        
+        const sheetLazy = (
+            sheet
+            ?
+            { sheet } // inexpensive operation
+            :
+            {
+                sheet: () => { // expensive operation & lazy
+                    // create a new sheet using our pre-configured `customJss`:
+                    const newSheet = createJssSheet(styles, sheetId);
+                    
+                    
+                    
+                    // register to `sheetManager` to be cached and also to be able to attach/detach to/from dom:
+                    sheetManager.add(sheetIdObj, newSheet);
+                    
+                    
+                    
+                    // here the ready to use sheet:
+                    // console.log('create sheet', sheetId);
+                    return newSheet;
+                }
+            }
         );
         
         
         
+        const [referenced, setReferenced] = useState(false);
         useIsomorphicLayoutEffect(() => {
+            if (!referenced) return; // not marked as referenced => do not manage
+            // console.log('ref sheet', sheetId);
+            
+            
+            
             // notify `sheetManager` that the `sheet` is being used
             // the `sheetManager` will attach the `sheet` to dom if one/more `sheet` users exist.
             sheetManager.manage(sheetIdObj);
@@ -86,12 +104,22 @@ export const createUseJssSheet = <TClassName extends ClassName = ClassName>(styl
                 // the `sheetManager` will detach the `sheet` from dom if no `sheet` user exists.
                 sheetManager.unmanage(sheetIdObj);
             };
-        }, []);
-
-
-
+        }, [referenced]);
+        
+        
+        
         // here the ready to use `sheet`'s classes:
-        return sheet.classes;
+        return (new Proxy(sheetLazy, {
+            get: (_unusedObj, propName: string) => {
+                if (propName === '$$typeof') return undefined; // react runtime type check
+                
+                
+                
+                if (typeof(sheetLazy.sheet) === 'function') sheetLazy.sheet = sheetLazy.sheet();
+                if (!referenced) setReferenced(true);
+                return (sheetLazy.sheet.classes as any)[propName];
+            }
+        }) as unknown as Classes<TClassName>);
     };
 }
 export const createUseSheet    = <TClassName extends ClassName = ClassName>(classes: ProductOrFactory<ClassList<TClassName>>, sheetId?: string): Factory<Classes<TClassName>> => {
