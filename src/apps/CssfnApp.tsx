129dac11b24d8445fb5781e14fe7c2d9a8505d9b
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-01-11 21:17:11 +0700

    updated CssfnApp

--- src/apps/CssfnApp.tsx
+++ src/apps/CssfnApp.tsx
@@ -6,12 +6,30 @@ import {
 // cssfn:
 import {
     // createStyle,
-    createJssSheet,
+    createSheet,
+    layout,
+    children,
+    mainComposition,
+    imports,
+    composition,
+    globalDef,
+    variants,
     // createCssfnStyle,
     // global,
     Style,
+    atRoot,
+    vars,
+    rules,
+    rule,
+    atGlobal,
+    fontFace,
+    keyframes,
     // Styles,
 }                           from '../libs/cssfn'           // cssfn core
+import {
+    breakpoints,
+    isScreenWidthAtLeast,
+}                           from '../libs/breakpoints'
 
 
 
@@ -43,32 +61,156 @@ const keyframesFadeOut = {
     },
 };
 
+// const testChildren = children(['.boo', '.foo'], [
+//     layout({
+//         background: 'pink',
+//     }),
+// ]);
 
-createJssSheet({
-    // @global:
-    '': {
-        '&:root': {
-            '--hello': 'world',
-            extend: [
-                { '--hi': 'arnold', } as Style
-            ],
-        } as Style,
+createSheet(() => [
+    // globalDef([
+    //     rule(':root', [
+    //         layout({
+    //             background: 'white',
+    //         }),
+    //     ]),
+    // ]),
+    mainComposition([
+        // imports([
+        //     composition([
+        //         layout({
+        //             ...children('span', [
+        //                 layout({
+        //                     border: 'pink',
+        //                 }),
+        //             ]),
+        //         }),
+        //     ]),
+        // ]),
+        // layout({
+        //     ...children('span', [
+        //         layout({
+        //             color: 'red',
+        //         }),
+        //     ]),
+        //     color: 'black',
+        //     // ...testChildren,
+        //     ...children(['.boo', '.foo'], [
+        //         layout({
+        //             background: 'pink',
+        //         }),
+        //     ]),
+        //     ...children(['&', 'span', 'aside'], [
+        //         layout({
+        //             textAlign: 'center',
+        //         }),
+        //     ]),
+        //     ...children(['hello', 'world'], [
+        //         layout({
+        //             say: 'hello',
+        //         }),
+        //     ]),
+        //     ...children(['hello', 'world'], [
+        //         layout({
+        //             bye: 'world',
+        //         }),
+        //     ]),
+        // }),
+        rules([
+            // rule('.rr1', [
+            //     layout({
+            //         test: 'rule 1',
+            //     }),
+            // ]),
+            // rule(['.rr1', ':rr2', ':disabled'], [
+            //     layout({
+            //         test: 'rule 2',
+            //     }),
+            // ]),
+            // rule(['.rr5', ':rr6', '&'], [
+            //     layout({
+            //         test: 'rule 3',
+            //     }),
+            // ]),
+            atGlobal([
+                rule('.btn-glob', [
+                    layout({
+                        content: '"im in nested global"'
+                    }),
+                ]),
+                atRoot([
+                    vars({
+                        '--my-var-1': '"im should in nested global"'
+                    }),
+                    variants([
+                        isScreenWidthAtLeast('xxl', [
+                            layout({
+                                fontSize: 'xx-large',
+                            }),
+                        ]),
+                    ]),
+                ]),
+                fontFace({
+                    fontFamily: 'lucida console',
+                }),
+            ]),
+            fontFace({
+                fontFamily: 'consolas',
+            }),
+        ]),
+    ]),
+    globalDef([
+        rule('.btn-cool', [
+            layout({
+                content: '"im in main global"',
 
-        '@keyframes fadeIn': keyframesFadeIn,
-
-        extend: {
-            '@keyframes fadeOut': keyframesFadeOut,
-
-            extend: [
-                {
-                    extend: {
-                        '@keyframes fadeOutPlus': keyframesFadeOut,
-                    } as Style,
-                } as Style,
-            ]
-        } as Style,
-    } as Style,
-})
+                ...children('.child-1', [
+                    layout({
+                        ...children('.child-2', [
+                            rules([
+                                isScreenWidthAtLeast('xxl', [
+                                    layout({
+                                        fontSize: 'xxx-large',
+                                    }),
+                                ]),
+                            ]),
+                        ]),
+                    }),
+                ]),
+            }),
+            variants([
+                isScreenWidthAtLeast('lg', [
+                    layout({
+                        content: '"im very cool"',
+                    }),
+                ]),
+            ]),
+        ]),
+        atRoot([
+            vars({
+                '--my-var-2': '"im should in main global"'
+            }),
+            variants([
+                isScreenWidthAtLeast('lg', [
+                    layout({
+                        fontSize: 'x-large',
+                    }),
+                ]),
+            ]),
+        ]),
+        fontFace({
+            fontFamily: 'arial narrow',
+        }),
+        keyframes('fade-out', {
+            from: {
+                background: 'red',
+            },
+            to: {
+                background: 'blue',
+            },
+        })
+    ]),
+])
 .attach();
 
 // var glob = global([
