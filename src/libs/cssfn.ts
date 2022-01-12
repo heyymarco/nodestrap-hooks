8ce40dc6e948bb553fe6f98d120f4fa77b0f28bf
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-01-12 17:41:47 +0700

    updated cssfn

--- src/libs/cssfn.ts
+++ src/libs/cssfn.ts
@@ -756,10 +756,9 @@ export const rule = (selectors: SelectorCollection, styles: StyleCollection): Ru
 // shortcut rule items:
 export const atGlobal          = (ruleCollection: RuleCollection) => rule('@global'     , rules(ruleCollection));
 export const fontFace          = (styles: StyleCollection) => rule('@font-face'         , styles);
-export const keyframes         = (name: string, items: PropEx.Keyframes) => rule(`@keyframes ${name}`, rules([
-    Object.entries(items).map(([name, value]) => rule(name, value as Style)),
-    Object.getOwnPropertySymbols(items).map((sym) => rule(sym.description ?? '', items[sym as any] as Style)),
-]));
+export const keyframes         = (name: string, items: PropEx.Keyframes) => rule(`@keyframes ${name}`, layout(Object.fromEntries(
+    Object.entries(items).map(([key, frame]) => [Symbol(key), frame])
+) as Style));
 export const noRule            = (styles: StyleCollection) => rule('&'                  , styles);
 export const emptyRule         = ()                        => rule(null                 , null  );
 export const atRoot            = (styles: StyleCollection) => rule(':root'              , styles);
