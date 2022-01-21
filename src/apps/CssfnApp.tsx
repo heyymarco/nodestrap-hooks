d38bca63d7d81f691974a1b895863a61a9478434
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-01-21 21:33:05 +0700

    updated CssfnApp

--- src/apps/CssfnApp.tsx
+++ src/apps/CssfnApp.tsx
@@ -1,67 +1,61 @@
 // react (builds html using javascript):
 import {
     default as React,
+    useState,
 }                           from 'react'         // base technology of our cssfn components
 
 // cssfn:
 import {
     // createStyle,
     createSheet,
+    style,
     layout,
-    children,
+    // children,
     mainComposition,
     imports,
     composition,
     globalDef,
-    variants,
+    // variants,
     // createCssfnStyle,
     // global,
-    Style,
-    atRoot,
-    vars,
+    // Style,
+    // atRoot,
+    // vars,
     rules,
     rule,
-    nestedRule,
-    atGlobal,
-    fontFace,
+    states,
+    // nestedRule,
+    // atGlobal,
+    // fontFace,
     keyframes,
     // Styles,
-    isHover,
+    // isHover,
 }                           from '../libs/cssfn'           // cssfn core
-import {
-    breakpoints,
-    isScreenWidthAtLeast,
-}                           from '../libs/breakpoints'
+// import {
+//     breakpoints,
+//     isScreenWidthAtLeast,
+// }                           from '../libs/breakpoints'
+import {Range} from '../libs/Range';
 
 
 
-function App() {
-	return (
-        <div className="App">
-            test
-        </div>
-    );
-}
 
-export default App;
-
-
-const keyframesFadeIn = {
-    from: {
-        opacity: 0,
-    },
-    '100%': {
-        opacity: 0.8,
-    },
-};
-const keyframesFadeOut = {
-    from: {
-        opacity: 0.9,
-    },
-    '100%': {
-        opacity: 0.1,
-    },
-};
+// const keyframesFadeIn = {
+//     from: {
+//         opacity: 0,
+//     },
+//     '100%': {
+//         opacity: 0.8,
+//     },
+// };
+// const keyframesFadeOut = {
+//     from: {
+//         opacity: 0.9,
+//     },
+//     '100%': {
+//         opacity: 0.1,
+//     },
+// };
 
 // const testChildren = children(['.boo', '.foo'], [
 //     layout({
@@ -84,77 +78,6 @@ const setSomeSome = () => composition([
     ]),
 ]);
 
-createSheet(() => [
-    mainComposition([
-        layout({
-            ...children(['::before', '::after'], {
-                color: 'red',
-            })
-        })
-        // imports([
-        //     layout({
-        //         color: 'red',
-        //     }),
-        //     layout({
-        //         color: null,
-        //     }),
-        // ]),
-        // rules([
-        //     isHover([
-        //         layout({
-        //             opacity: 0.5,
-        //             color: 'red',
-        //         }),
-        //     ]),
-        //     isHover([
-        //         layout({
-        //             color: 'blue',
-        //             display: 'grid',
-        //         }),
-        //     ]),
-        //     rule(':focus', [
-        //         layout({
-        //             background: 'pink',
-        //         }),
-        //     ]),
-        // ])
-        // rules([
-        //     atGlobal([
-        //         rule('.btn', [
-        //             layout({
-        //                 background: 'gray',
-        //                 // ...nestedRule('foo', [
-        //                 //     layout({
-        //                 //     }),
-        //                 // ]),
-        //             }),
-        //         ]),
-        //     ]),
-        // ]),
-    ]),
-    // globalDef([
-    //     keyframes('fade-out', {
-    //         from: {
-    //             background: 'red',
-    //             filter: [[
-    //                 'opacity(0.5)',
-    //                 'brightness(85%)',
-    //                 'contrast(50%)',
-    //             ]],
-    //             boxShadow: [
-    //                 'aaa',
-    //                 'bbb',
-    //                 'ccc',
-    //             ],
-    //         },
-    //         to: {
-    //             background: 'blue',
-    //         },
-    //     })
-    // ]),
-])
-.attach();
-
 // var glob = global([
 //     [ '.big',
 //         [{
@@ -175,3 +98,217 @@ createSheet(() => [
 //     ],
 // ]);
 // console.log(glob);
+
+export default function Page() {
+    const [specificityWeight, setSpecificityWeight] = useState<number>(-1);
+
+    const sheet = createSheet(() => [
+        mainComposition([
+            // style({
+            //     ...imports([
+            //         style({
+            //             ...imports([
+            //                 {
+            //                     '--test-deep-import': 0,
+            //                 },
+            //             ]),
+            //             ...style({
+            //                 '--test-import-style-1': 0,
+            //             }),
+            //             ...style({
+            //                 '--test-import-style-2': 0,
+            //             }),
+            //             '--test-import-style-x': 0,
+            //         }),
+            //         {
+            //             '--test-import': 0,
+            //         }
+            //     ]),
+            //     ...style({
+            //         '--test-1': 0,
+            //     }),
+            // }),
+            // layout({
+            //     ...children(['::before', '::after'], {
+            //         color: 'red',
+            //     })
+            // })
+            // imports([
+            //     layout({
+            //         color: 'red',
+            //     }),
+            //     layout({
+            //         color: null,
+            //     }),
+            // ]),
+            // rules([
+            //     rule([
+            //         '.passive',
+            //     ], [
+            //         layout({
+            //             background: 'pink',
+            //         }),
+            //     ]),
+                
+            //     rule([
+            //         ['.active', ':checked:not(.actived)'],
+            //     ], [
+            //         layout({
+            //             background: 'pink',
+            //         }),
+            //     ]),
+                
+            //     rule([
+            //         '.passive',
+            //         ':is(.active, .actived)',
+            //         // ['.active', ':checked:not(.actived)'],
+            //     ], [
+            //         layout({
+            //             background: 'pink',
+            //         }),
+            //     ]),
+                
+            //     rule([
+            //         ['.active', ':checked:not(.actived)'],
+            //     ], [
+            //         layout({
+            //             background: 'pink',
+            //         }),
+            //     ]),
+            // ], { minSpecificityWeight: 3 }),
+            // style({
+            //     display: 'grid',
+            //     appearance: 'none',
+
+            //     ...states([
+            //         rule([':disabled', '.disable', '.disabled'], {
+            //             opacity: 0.5,
+            //         }),
+            //     ]),
+            //     ...rule([':enabled', '.enabling'], {
+            //         opacity: 0.95,
+            //     }),
+            // }),
+            states([
+                rule([
+                    ':wohaa',
+                    ':weleh',
+                    '::before',
+                    '::after'
+                ], [
+                    layout({
+                        color: 'pink',
+                    }),
+                ]),
+
+                // rule([
+                //     '&>.one',
+                // ], [
+                //     layout({
+                //         color: 'pink',
+                //     }),
+                // ]),
+                // rule([
+                //     ['&>.one', '&>.other', '::before'],
+                // ], [
+                //     layout({
+                //         color: 'pink',
+                //     }),
+                // ]),
+                // rule([
+                //     ['&>.one', '::before', '::after'],
+                // ], [
+                //     layout({
+                //         color: 'pink',
+                //     }),
+                // ]),
+    
+                // rule([
+                //     '.one>&',
+                // ], [
+                //     layout({
+                //         color: 'red',
+                //     }),
+                // ]),
+                // rule([
+                //     ['.one>&', '.other>&', '::before'],
+                // ], [
+                //     layout({
+                //         color: 'red',
+                //     }),
+                // ]),
+                // rule([
+                //     ['.one>&', '::before', '::after'],
+                // ], [
+                //     layout({
+                //         color: 'red',
+                //     }),
+                // ]),
+                
+                // rule([
+                //     '.one.two.three.four.five',
+                // ], [
+                //     layout({
+                //         color: 'pink',
+                //     }),
+                // ]),
+                // rule([
+                //     ['.one.two.three.four.five', '::before'],
+                // ], [
+                //     layout({
+                //         color: 'pink',
+                //     }),
+                // ]),
+                // rule([
+                //     ['.one.two.three.four.five', '::before', '::after:first-child'],
+                // ], [
+                //     layout({
+                //         color: 'pink',
+                //     }),
+                // ]),
+                
+                // rule([
+                //     [':one:two.tree', ':ONE:TWO:THREEE:not(.FOUR)'],
+                // ], [
+                //     layout({
+                //         background: 'pink',
+                //     }),
+                // ]),
+            ]
+            // , { specificityWeight: 4 }
+            )
+            // rules([
+            //     atGlobal([
+            //         rule('.btn', [
+            //             layout({
+            //                 background: 'gray',
+            //                 ...nestedRule('&', [
+            //                     layout({
+            //                         color: 'magenta',
+            //                     }),
+            //                 ]),
+            //             }),
+            //         ]),
+            //     ]),
+            // ]),
+        ]),
+    ]);
+
+    
+    return (
+        <>
+            <pre dangerouslySetInnerHTML={{ __html: sheet.toString()}} />
+            
+            <hr />
+
+            {/* <Range
+                theme='primary'
+
+                min={-1} max={5} value={specificityWeight}
+                onChange={(e) => {
+                    setSpecificityWeight(e.currentTarget.valueAsNumber);
+                }}
+            /> */}
+        </>
+    );
+}
