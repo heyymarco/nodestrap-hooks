79bc5be9b777c592b9c4ecc4594b1fbf6b44654f
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-03-23 02:20:12 +0700

    updated Label

--- src/libs/Label.tsx
+++ src/libs/Label.tsx
@@ -57,10 +57,6 @@ import {
     usesContentBasicLayout,
     usesContentBasicVariants,
 }                           from './Content'
-import {
-    // hooks:
-    usesThemeDefault,
-}                           from './Control'
 
 
 
@@ -86,9 +82,6 @@ export const usesLabelLayout = () => {
         ...imports([
             // layouts:
             usesBasicLayout(),
-            
-            // colors:
-            usesThemeDefault(),
         ]),
         ...style({
             // layouts:
@@ -206,6 +199,7 @@ export function Label<TElement extends HTMLElement = HTMLElement>(props: LabelPr
             
             
             // variants:
+            theme={props.theme ?? 'secondary'}
             mild={props.mild ?? true}
             
             
